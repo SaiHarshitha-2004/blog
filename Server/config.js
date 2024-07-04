74d4ad788c172ed4c167cbce60b43b60b5edeefb
SaiHarshitha-2004 <108074739+SaiHarshitha-2004@users.noreply.github.com>
2024-07-04 08:43:51 +0530

    Update config.js

--- Server/config.js
+++ Server/config.js
@@ -6,4 +6,5 @@ dotenv.config();
 export const USERNAME = process.env.VITE_MONGODB_USERNAME;
 export const PASSWORD = process.env.VITE_MONGODB_PASSWORD;
 export const GOOGLE_ID = process.env.VITE_GOOGLE_ENGINE_ID ;
+export const ENVPORT = process.env.VITE_PORT;
 export const GOOGLE_API_KEY = process.env.VITE_GOOGLE_API_KEY ;
